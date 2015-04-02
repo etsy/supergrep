37bc01639995eda6b8fc56e3382264527be87fb3
C. Regis Wilson <rwilson@truecar.com>
2015-04-02 16:40:08 -0700

    Attempt to fix old dependencies on express (see http://stackoverflow.com/questions/23611771/nodejs-throws-error-while-calling-createserver)

--- stream.js
+++ stream.js
@@ -8,6 +8,10 @@ var socketio    = require('socket.io');
 var less        = require('less');
 var uglify      = require('uglify-js');
 var LogReader   = require('./lib/logreader');
+var http        = require('http');
+var logger      = require('morgan');
+var bodyParser  = require('body-parser');
+var errorhandler = require('errorhandler')
 
 var STATIC_PATH = '/static';
 
@@ -56,13 +60,14 @@ var cache = { js: {}, jsc: {}, less: {} };
         }
     }
 
-var app = express.createServer();
+var app = express();
+var server = http.createServer(app);
 //Allow JSONP support
 app.enable("jsonp callback");
-app.use(express.logger());
-app.use(express.bodyParser());
+app.use(logger);
+app.use(bodyParser);
 app.use(express.query());
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.use(errorhandler({ dumpExceptions: true, showStack: true }));
 
 //IRCCat proxy
 app.post('/irccat', function (req, res, next) {
@@ -245,7 +250,7 @@ app.all('/v2/:respath?', function (req, res, next) {
         (qs.length ? '?' + qs.join('?') : '')
         );
 });
-app.use(express.staticCache());
+// app.use(express.staticCache());
 app.use(express.static(__dirname + STATIC_PATH));
 server = app.listen(config.port);
 
